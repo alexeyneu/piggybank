5f0eb1f05f48aaff7a887def0aa23de810cd4b8a
alex <alexeyneu@yahoo.com>
2024-05-15 13:47:16 +0300

    n,n

--- piggybank_extended.sol
+++ piggybank_extended.sol
@@ -1,21 +1,25 @@
 pragma solidity ^0.7.2;
 pragma experimental ABIEncoderV2;
-contract piggyb_extended_v4
+contract piggyb_extended_v5
 {
 	address payable whose;
 	uint unl = type(uint).max;
 	bool rush = false;
+	uint barcode = 1;
+	event NewPiggybank(uint barc);
+	event PiggybankBroken(uint barc);
 
-	constructor() public
+	constructor()
 	{
 		whose = msg.sender;
 	}
-	fallback() external payable
+	receive() external payable
 	{
 		if (rush == false)
 		{
 			unl = block.timestamp + 4 days;
 			rush = !false;
+			emit NewPiggybank(barcode);
 		}
 	}
 	function bankbreak() public
@@ -24,5 +28,7 @@ contract piggyb_extended_v4
 		whose.transfer(address(this).balance);
 		rush = false;
 		unl = type(uint).max;
+		barcode = barcode + 1;
+		emit PiggybankBroken(barcode - 1);
 	}
 }
